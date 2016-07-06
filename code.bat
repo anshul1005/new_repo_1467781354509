95b979c9f7b5b1d3870b09f895072651b317571d
Ruchit Pawar <ruchitpawar@QAIT.COM>
2016-07-06 10:32:40 +0530

    Initial commit

---
+++ code.bat
@@ -0,0 +1,7 @@
+cd \
+git clone https://github.com/anshul1005/new_repo_1467781354509.git
+copy C:\Users\ruchitpawar\Desktop\github\code.bat new_repo_1467781354509
+cd new_repo_1467781354509
+git add -A
+git commit -m "Initial commit"
+git push origin master
